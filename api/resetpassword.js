57c9555850afa87760a60ddca53a39cab292a9f0
gmanmesh <140646198+gmanmesh@users.noreply.github.com>
2025-09-19 13:13:56 -0500

    Update resetpassword.js

--- api/resetpassword.js
+++ api/resetpassword.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: 'Token and password are required' });
     }
     try {
-        const { user, error } = await supabase.auth.api.updateUser(token,{ password });
+        const { data, error } = await supabase.auth.updateUser({ password: password });
         if (error) {
             return res.status(400).json({success: false, error: error.message });
         }
