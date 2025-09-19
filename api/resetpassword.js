1a62025eaea02d37a27420cf8533e88aa4625026
gmanmesh <140646198+gmanmesh@users.noreply.github.com>
2025-09-19 15:36:46 -0500

    Update resetpassword.js

--- api/resetpassword.js
+++ api/resetpassword.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     }
     try {
         // Verify the token and get the user ID
-    const { user, error: userError } = await supabase.auth.api.getUser(access_token);
+    const { user, error: userError } = await supabase.auth.api.getUser(token);
     if (userError || !user) {
       return res.status(400).json({ error: 'Invalid or expired token' });
     }
