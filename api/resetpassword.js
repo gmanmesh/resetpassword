fb6015c427cae3f780eed1bb5b0f009adc2df762
gmanmesh <140646198+gmanmesh@users.noreply.github.com>
2025-09-19 16:07:15 -0500

    Update resetpassword.js

--- api/resetpassword.js
+++ api/resetpassword.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: 'Invalid or expired token' });
     }
     // Update user password as admin
-    const { data, error } = await supabaseAdmin.auth.admin.updateUser(user.id, {
+    const { data, error } = await supabaseAdmin.auth.updateUser(user.id, {
       password: new_password,
     });
         if (error) {
