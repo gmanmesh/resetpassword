8365b56427e29282f608e3ef2d7a1b8f21f7bd08
gmanmesh <gebreman128@gmail.com>
2025-09-19 12:57:07 -0500

    Add new file to the folder

---
+++ api/resetpassword.js
@@ -0,0 +1,29 @@
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl=process.env.SUPABASE_URL;
+const secretKey=process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+const supabase = createClient(supabaseUrl, secretKey);
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+    const { token, password } = req.body;
+
+    if (!token || !password) {
+        return res.status(400).json({ error: 'Token and password are required' });
+    }
+    try {
+        const { data, error } = await supabase.auth.api.updateUser(token, { password })
+        if (error) {
+            return res.status(400).json({success: false, error: error.message });
+        }
+        return res.status(200).json({ success: true, message: 'Password updated successfully' });
+    }
+    catch (error) {
+        return res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+}
